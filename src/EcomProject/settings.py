55f48ee624f43edabda2cf6b211f72a3dd549af1
Yannick KIKI <seyive.kiki@gmail.com>
2019-03-18 18:30:01 +0100

    Home page

--- src/EcomProject/settings.py
+++ src/EcomProject/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files (uploadeds, ...)
+MEDIA_URL = '/'
+
+MEDIA_ROOT = '/home/yannick/projects/django_projects/EcomProject/src/'
--- src/e_shop/views.py
+++ src/e_shop/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from e_shop.models import Product
 # Here are our views.
 
 def home(request):
-    return render(request,'e_shop/home.html')
+    p = Product.objects.all()[:4] #take four first products
+    return render(request,'e_shop/home.html',{'products':p})
 
 def shop(request):
     return render(request,'e_shop/shop.html')
